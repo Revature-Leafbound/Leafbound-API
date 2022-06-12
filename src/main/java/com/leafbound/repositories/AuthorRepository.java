2e153720f284495071dbff6434df7520c895113c
FullSol <ceraines@student.fullsail.edu>
2022-06-12 17:59:46 -0400

    Create Author Service

---
+++ src/main/java/com/leafbound/repositories/AuthorRepository.java
@@ -0,0 +1,18 @@
+package com.leafbound.repositories;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public interface AuthorRepository {
+    /*
+     * Auto-generated methods
+     * save() (insert)
+     * update() (update)
+     * delete() (delete)
+     * findAll() (select all)
+     * findById() (select by id)
+     */
+}
---
+++ src/main/java/com/leafbound/services/AuthorService.java
@@ -0,0 +1,31 @@
+package com.leafbound.services;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.leafbound.models.Author;
+
+@Service
+@Transactional
+public interface AuthorService {
+    /*
+     * Auto-generated methods
+     * save() (insert)
+     * update() (update)
+     * delete() (delete)
+     * findAll() (select all)
+     * findById() (select by id)
+     */
+    public boolean add();
+
+    public Author getById();
+
+    public List<Author> getAll();
+
+    public boolean edit();
+
+    public boolean remove();
+}
