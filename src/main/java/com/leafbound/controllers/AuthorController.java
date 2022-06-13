65fbc9c7aec12eabcea3293bbc9baca2259ab899
FullSol <ceraines@student.fullsail.edu>
2022-06-12 22:24:16 -0400

    Cleaned up imports

--- src/main/java/com/leafbound/controllers/AuthorController.java
+++ src/main/java/com/leafbound/controllers/AuthorController.java
@@ -2,11 +2,8 @@ package com.leafbound.controllers;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
