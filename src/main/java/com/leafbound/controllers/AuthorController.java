152eb97dde33c5a7e74845b0e39cf4d0f283f7d9
FullSol <ceraines@student.fullsail.edu>
2022-06-13 09:09:28 -0400

    Changed cross origin to restict to localhost only for the time being.

--- src/main/java/com/leafbound/controllers/AuthorController.java
+++ src/main/java/com/leafbound/controllers/AuthorController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +20,7 @@ import com.leafbound.services.AuthorServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-@CrossOrigin(origins = { "*" })
+@CrossOrigin(origins = { "http://localhost:8080" })
 @RestController
 @RequestMapping("/api/v1")
 @Api(value = "Author Collection", tags = "AUTHOR_TAG") // TODO: Figure out the tag thing
