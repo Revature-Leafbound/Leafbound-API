741a823778c00cc2a540adbd0f2d5052708c8f08
FullSol <ceraines@student.fullsail.edu>
2022-06-12 23:11:23 -0400

    completed base configuration of application

---
+++ src/main/java/com/leafbound/config/JPAConfiguration.java
@@ -0,0 +1,14 @@
+package com.leafbound.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories("com.leafbound.repositories")
+@EnableJpaAuditing
+@EnableTransactionManagement
+public class JPAConfiguration {
+
+}
