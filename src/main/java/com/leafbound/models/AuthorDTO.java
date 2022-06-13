622587f5636794550bf41459ede44ac057350557
FullSol <ceraines@student.fullsail.edu>
2022-06-12 21:20:25 -0400

    created a DTO
    This was promted by SonarCloud warning

---
+++ src/main/java/com/leafbound/models/AuthorDTO.java
@@ -0,0 +1,46 @@
+package com.leafbound.models;
+
+public class AuthorDTO {
+    private int id;
+    private String name;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+
+    @Override
+    public String toString() {
+        return "AuthorDTO [id=" + id + ", name=" + name + "]";
+    }
+
+}
