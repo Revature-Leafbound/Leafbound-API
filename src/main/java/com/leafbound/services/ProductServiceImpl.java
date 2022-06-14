5008a78cca1ea3bf462078ad1955d32ec44af924
jgalena <jgalena1000@gmail.com>
2022-06-14 09:31:42 -0500

    Added imports to fix errors

--- src/main/java/com/leafbound/services/ProductServiceImpl.java
+++ src/main/java/com/leafbound/services/ProductServiceImpl.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.leafbound.models.Product;
+import com.leafbound.repositories.ProductRepository;
+
 
 @Service
 @Transactional
