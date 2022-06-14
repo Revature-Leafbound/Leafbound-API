558291d39cc2d2f3df0f6cc8fe1d88b68a9bbb38
jgalena <jgalena1000@gmail.com>
2022-06-14 09:46:33 -0500

    Added imports to fix erorrs

--- src/main/java/com/leafbound/controllers/ProjectController.java
+++ src/main/java/com/leafbound/controllers/ProjectController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.leafbound.models.Product;
 import com.leafbound.services.ProductService;
 
 import io.swagger.annotations.Api;
