859d1ef9778ea59cb078a3762aa3b781e77e0b80
briiitelord <96019784+briiitelord@users.noreply.github.com>
2022-06-14 11:45:12 -0400

    completed product service test - may need to be edited to match naming conventios

--- src/test/java/com/leafbound/test/services/ProductServiceTest.java
+++ src/test/java/com/leafbound/test/services/ProductServiceTest.java
@@ -1,5 +1,8 @@
 package com.leafbound.test.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
@@ -23,6 +26,10 @@ import org.springframework.stereotype.Service;
 
 import com.leafbound.models.Product;
 import com.leafbound.repositories.ProductRepository;
+import com.leafbound.services.ProductService;
+import com.leafbound.services.ProductServiceImpl;
+
+
 
 
 
@@ -66,15 +73,113 @@ public class ProductServiceTest {
 
     @Test
     @Order(2)
-    @DisplayName("2. Create Product Test")
-    public void createProductTest_success(){
+    @DisplayName("2. Passed Create Product Test")
+    public void addProductTest_success(){
         mockProduct3 = mockProduct2;
         mockProduct3.setid(3);
 
         when(productRepo.save(mockProduct3)).thenReturn(mockProduct3);
 
-        assertEquals(true, productService.createUser(mockProduct3));
+        assertEquals(true, productService.add(mockProduct3));
     }
+
+    @Test 
+    @Order(3)
+    @DisplayName("3. Failed Creation UserRole Test")
+    public void addProductTest_failure(){
+        mockProduct3 = new Product();
+        mockProduct3.setid(3);
+        when(productRepo.save(mockProduct3)).thenReturn(mockProduct3);
+
+        assertEquals(false, productService.add(mockProduct3));
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("4. Passed - Get Product based on ID Test")
+    public void getProductByIDTest_success(){
+
+        when(productRepo.getProductById(1)).thenReturn(mockProduct1);
+
+        assertEquals(mockProduct1, productService.getProductById(1));
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("5. Failed - Get Product based on ID Test")
+    public void getProductByIDTest_failure(){
+
+        when(productRepo.getProductById(1)).thenReturn(mockProduct2);
+
+        assertNotEquals(mockProduct1, productService.getProductById(1));
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("5. Get all Products - success")
+    public void getAllProductTest_success(){
+        when(productService.getAll()).thenReturn(dummyDb);
+
+        assertEquals(dummyDb, productService.getAll());
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("6. Get all Products - failure")
+    public void getAllProductTest_failure(){
+        when(productService.getAll()).thenReturn(null);
+
+        assertNotEquals(dummyDb, productService.getAll());
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("7. Update product - success") 
+        public void updateProduct_success(){
+            mockProduct2.setIsbn("newIsbn");
+
+            when(productService.getById(2)).thenReturn(mockProduct2);
+            when(productRepo.save(mockProduct2)).thenReturn(mockProduct2);
+
+            assertEquals(true, productService.update(mockProduct2));
+        }
+    
+
+
+    @Test
+    @Order(8)
+    @DisplayName("8. Update product - failure") 
+        public void updateProduct_failure(){
+            mockProduct2.setIsbn("ISBN2");
+
+            when(productService.getById(2)).thenReturn(mockProduct2);
+            when(productRepo.save(mockProduct2)).thenReturn(mockProduct2);
+
+            assertFalse(productService.update(mockProduct2));
+        }
+    
+
+    
+    
+
+    @Test
+    @Order(9)
+    @DisplayName("9. Delete Product Test - success") 
+        public void deleteProductById_success(){
+            when(productRepo.delete(2)).thenReturn(true);
+            assertEquals(true, productService.remove(mockProduct2.getById(2)));
+        }
+    
+
+    @Test
+    @Order(10)
+    @DisplayName("10. Delete Product Test - failure") 
+        public void deleteProductById_failure(){
+            when(productRepo.delete(2)).thenReturn(false);
+            assertNotNull(productService.getById(2));
+        }
+    
+
 
     
 
