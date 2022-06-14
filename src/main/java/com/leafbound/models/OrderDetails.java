92578850537fda8ce4406c1fc1279409763fb0a3
pyaeger <patrick540@revature.net>
2022-06-14 18:05:05 -0400

    adding code for OrderDetails model

--- src/main/java/com/leafbound/models/OrderDetails.java
+++ src/main/java/com/leafbound/models/OrderDetails.java
@@ -21,11 +21,55 @@ import lombok.Data;
 @Entity
 @Table(name="order_details")
 @Data
-@ApiModel(value="OrderDetails", description="This model serves as the basic model for all orderDetails entity API operations.")
+@ApiModel(value="OrderDetails", description="This model serves as the basic model for all OrderDetails entity API operations.")
 public class OrderDetails {
 	
-// initial commit to begin work on OrderDetails model.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @ApiModelProperty(name = "id", value = "An integer value that serves as the unique identifier for any order details entity.", required = true)
+    private int id;
 
-} OrderDetails {
+    @OneToOne
+    @JoinColumn(name = "product_id", unique = true, nullable = false)
+    @ApiModelProperty(name = "order_details_product_id", value = "An integer value that serves as the product id for the product.", required = true)
+    private Products product;
+
+    @Column(name = "quantity", nullable = false)
+    @ApiModelProperty(name = "quantity", value = "An integer value describing the quantity of a product.", required = true)
+    private int quantity;
+
+    @OneToOne
+    @JoinColumn(name = "order_id", unique = true, nullable = false)
+    @ApiModelProperty(name = "order_details_order_id", value = "An integer value that serves as the order id for the order.", required = true)
+    private User user;
+
+
+    public OrderDetails() {
+        super();
+    }
+
+
+    public OrderDetails(int id, Products product, int quantity, Orders order) {
+        this.id = id;
+        this.product = product;
+        this.quantity = quantity;
+        this.order = order;
+    }
+
+
+
+    public OrderDetails(Products product, int quantity, Order order) {
+        this.product = product;
+        this.quantity = quantity;
+        this.order = order;
+    }
+
+    /**
+     * @param id
+     * @param product
+     * @param quantity
+     * @param order
+     */
     
 }
