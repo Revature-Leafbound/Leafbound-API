b7eebdb78ae664fc8df760c807d5cafbb805562a
CuChulainn64 <patrick.miller1@snhu.edu>
2022-06-14 16:57:39 -0400

    removed unnecessary anotations

--- src/main/java/com/leafbound/controllers/UserController.java
+++ src/main/java/com/leafbound/controllers/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leafbound.models.User;
@@ -32,7 +31,7 @@ public class UserController {
 	private UserService service;
 	
 	@PostMapping("/CreateUser")
-	public @ResponseBody String createUser(@RequestBody User user) {
+	public String createUser(@RequestBody User user) {
 		log.info("Creating user");
 		
 		return (service.createUser(user)) ? "User created successfully": "Error creating user";
@@ -40,19 +39,19 @@ public class UserController {
 	}
 
 	@GetMapping("/GetUser")
-	public @ResponseBody User readById(UUID id) {
+	public User readById(UUID id) {
 		log.info("Getting user with id: " + id);
 		return service.getUserById(id);
 	}
 	
 	@GetMapping("/GetUsers")
-	public @ResponseBody List<User> readAllUsers(){
+	public List<User> readAllUsers(){
 		log.info("Getting all users");
 		return service.getAllUsers();
 	}
 	
 	@PatchMapping("/UpdateUser")
-	public @ResponseBody String updateUser(@RequestBody User user) {
+	public String updateUser(@RequestBody User user) {
 		log.info("Updating user");		
 		
 		return (service.updateUser(user)) ? "Update successful": "Update failed";
@@ -60,21 +59,21 @@ public class UserController {
 	}
 	
 	@DeleteMapping("/DeleteUser")
-	public @ResponseBody String deleteUser(@RequestBody UUID id) {
+	public String deleteUser(@RequestBody UUID id) {
 		log.info("Deleting user");	
 		return (service.deleteUser(id)) ? "Delete successful": "Delete failed";
 		
 	}
 	
 	@GetMapping("/Login")
-	public @ResponseBody User login(@RequestBody User user) {
+	public User login(@RequestBody User user) {
 		log.info("Loggin in");	
 		return service.login(user);
 		
 	}
 	
 	@PostMapping("/Register")
-	public @ResponseBody User register(@RequestBody User user) {
+	public User register(@RequestBody User user) {
 		log.info("Register user");	
 		return service.register(user);
 		
