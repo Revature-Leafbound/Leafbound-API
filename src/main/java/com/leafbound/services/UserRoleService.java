a89825384dac91208342277b5a6eea7ff5860edf
FullSol <48437419+FullSol@users.noreply.github.com>
2022-06-15 16:59:43 -0400

    Update UserRoleService.java

--- src/main/java/com/leafbound/services/UserRoleService.java
+++ src/main/java/com/leafbound/services/UserRoleService.java
@@ -6,13 +6,9 @@ import java.util.List;
 
 public interface UserRoleService {
 
-    public List<UserRole> getAll();
-
     public boolean add(UserRole userRole);
 
     public UserRole getById(int id);
-
-    //public boolean edit(int id, UserRole userRole);
 
     public boolean remove(int id);
 
