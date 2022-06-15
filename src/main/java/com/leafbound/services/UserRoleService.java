bb1b100fcf34328b1927ad20a57f6d156428e8ef
L3viathon <lvmchoi@gmail.com>
2022-06-15 06:17:43 -0700

    Updated UserRole Service and ServiceImpl

--- src/main/java/com/leafbound/services/UserRoleService.java
+++ src/main/java/com/leafbound/services/UserRoleService.java
@@ -1,3 +1,19 @@
+package com.leafbound.services;
+
+package com.leafbound.models.UserRole;
+
+import java.util.List;
+
 public interface UserRoleService {
-    
+
+    public List<UserRole> getAll();
+
+    public boolean add(UserRole userRole);
+
+    public UserRole getById(int id);
+
+    //public boolean edit(int id, UserRole userRole);
+
+    public boolean remove(int id);
+
 }
--- src/main/java/com/leafbound/services/UserRoleServiceImpl.java
+++ src/main/java/com/leafbound/services/UserRoleServiceImpl.java
@@ -1,3 +1,102 @@
-public class UserRoleServiceImpl {
-    
+package com.leafbound.services;
+
+import java.util.List;
+
+import com.leafbound.models.UserRole;
+import com.leafbound.repositories.UserRoleRepository;
+
+@Service
+public class UserRoleServiceImpl implements UserRoleService {
+
+    @Autowired
+    private UserRoleRepository repository;
+
+    @Override
+    public List<UserRole> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public UserRole getById(int id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public boolean add(UserRole userRole) {
+        int pk = repository.save(userRole).getId();
+        return (pk > 0) ? true : false;
+    }
+
+    // @Override
+    // public boolean edit(int id, UserRole userRole) {
+    //     UserRole target = repository.findById(id);
+    //     target.setRole(userRole.getRole());
+    //     return (repository.save(target) != null) ? true : false;
+    // }
+
+    @Override
+    //@Transactional(rollbackFor = Exception.class)
+    public boolean remove(int id) {
+        try {
+            repository.delete(id);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+
 }
+
+/*
+
+import java.util.List;
+
+import com.amplifier.models.UserRole;
+import com.amplifier.repositories.UserRolesRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserRolesServiceImpl implements UserRolesService {
+
+    @Autowired
+    private UserRolesRepository repository;
+
+    public UserRolesServiceImpl(UserRolesRepository repo) {
+    }
+
+    @Override
+    public boolean add(UserRole userRole) {
+        int pk = repository.save(userRole).getId();
+        return (pk > 0) ? true : false;
+    }
+
+    @Override
+    public UserRole getById(int id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<UserRole> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public boolean edit(int id, UserRole userRole) {
+        UserRole target = repository.findById(id);
+        target.setRole(userRole.getRole());
+        return (repository.save(target) != null) ? true : false;
+    }
+
+    @Override
+    //@Transactional(rollbackFor = Exception.class)
+    public boolean remove(int id) {
+        try {
+            repository.delete(id);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    } */
