0a9d7b52942b4e1311e9f50175f724ebefedbed3
FullSol <48437419+FullSol@users.noreply.github.com>
2022-06-15 17:01:47 -0400

    Update UserRoleServiceImpl.java

--- src/main/java/com/leafbound/services/UserRoleServiceImpl.java
+++ src/main/java/com/leafbound/services/UserRoleServiceImpl.java
@@ -7,8 +7,10 @@ import com.leafbound.repositories.UserRoleRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class UserRoleServiceImpl implements UserRoleService {
 
     @Autowired
