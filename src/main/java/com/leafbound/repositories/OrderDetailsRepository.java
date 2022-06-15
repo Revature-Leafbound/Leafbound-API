87d2c078e3a43692b3d072d6ce3a9d215a5a8325
FullSol <ceraines@student.fullsail.edu>
2022-06-14 22:41:59 -0400

    created repository

---
+++ src/main/java/com/leafbound/repositories/OrderDetailsRepository.java
@@ -0,0 +1,11 @@
+package com.leafbound.repositories;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public class OrderDetailsRepository {
+
+}
