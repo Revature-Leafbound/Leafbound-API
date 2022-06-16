fd1c7852232aac99b1cb2910b419e24ddead0088
Jorge Ayestas <jorgeayestas@Jorges-MacBook-Pro.local>
2022-06-15 22:03:26 -0400

    Order Classes

--- src/main/java/com/leafbound/repositories/OrderRepository.java
+++ src/main/java/com/leafbound/repositories/OrderRepository.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.leafbound.models.Order;
@@ -13,5 +14,8 @@ import com.leafbound.models.Order;
 @Repository
 @Transactional
 public interface OrderRepository extends JpaRepository<Order, UUID>{
+	
+	@Query(value="SELECT * FROM orders where order_date=?1", nativeQuery = true)
+	public Order findByDate(Order order);
 
 }
