77b7ad991c120c4097a91138687143195b104ade
FullSol <ceraines@student.fullsail.edu>
2022-06-16 12:36:11 -0400

    updated product collection nae for api docs

--- src/main/java/com/leafbound/controllers/ProductController.java
+++ src/main/java/com/leafbound/controllers/ProductController.java
@@ -20,44 +20,42 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@RequestMapping("/api/v1")  // this might need to be changed
-@CrossOrigin(origins = {"http://localhost:5500/", "http://127.0.0.1:5500/", "http://127.0.0.1:5501/", "http://localhost:8080/", "http://127.0.0.1:8080/", "http://localhost:5502/", "http://127.0.0.1:5502/"})
-@Api(value = "ProductRestController", tags = {"REST controller related to Product Entities"})
+@RequestMapping("/api/v1") // this might need to be changed
+@CrossOrigin(origins = { "http://localhost:5500/", "http://127.0.0.1:5500/", "http://127.0.0.1:5501/",
+		"http://localhost:8080/", "http://127.0.0.1:8080/", "http://localhost:5502/", "http://127.0.0.1:5502/" })
+@Api(value = "ProductRestController", tags = { "PRODUCTS COLLECTION" })
 public class ProductController {
 
 	@Autowired
 	private ProductService productServ;
-	
-	
-	//get all products
+
+	// get all products
 	@GetMapping("/products")
-	@ApiOperation(value="Find all products")
+	@ApiOperation(value = "Find all products")
 	public @ResponseBody List<Product> getAll() {
-		
+
 		return productServ.getAllProducts();
 	}
-	
-	//create new product
+
+	// create new product
 	@PostMapping("/product")
-	@ApiOperation(value="create new product entity")
+	@ApiOperation(value = "create new product entity")
 	public @ResponseBody Product createProduct(@RequestBody Product product) {
 		return productServ.createProduct(product);
 
-		
 	}
-	
-	//Update product
+
+	// Update product
 	@PutMapping("/product")
-	@ApiOperation(value="update product entity")
+	@ApiOperation(value = "update product entity")
 	public @ResponseBody Product updateProduct(@RequestBody Product product) {
 		return productServ.updateProduct(product);
 
-		
 	}
-	
-	//Delete product
+
+	// Delete product
 	@DeleteMapping("/product")
-	@ApiOperation(value="update product entity")
+	@ApiOperation(value = "update product entity")
 	public @ResponseBody String deleteProduct(@RequestBody Product product) {
 		return productServ.deleteProduct(product) ? "DELETION_SUCCESSFUL" : "DELETION_FAILED";
 	}
