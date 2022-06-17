3a5add72f45b5c9eac0a0539b5f4d659237181cd
DragonLute1 <lucasadam04@gmail.com>
2022-06-16 21:45:38 -0400

    Start of Reflections API implimentation

--- src/test/java/com/leafbound/services/UserServiceTest.java
+++ src/test/java/com/leafbound/services/UserServiceTest.java
@@ -3,12 +3,13 @@ package com.leafbound.services;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
-
+import java.lang.reflect.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import org.apache.log4j.Logger;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
@@ -19,6 +20,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Qualifier;
 
@@ -33,37 +35,42 @@ import com.leafbound.services.UserRoleServiceImpl;
 @ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserServiceTest {
-	@Mock(name="UserRepo")
+	@Mock
 	private static UserRepository mockrepo;
 
+	@Mock
+	private static UserRoleRepository mockrole;
+	
+	
 	@InjectMocks
-	@Qualifier(value = "UserRepo")
 	private static UserServiceImpl uServ;
 	
 	private static User u1, u2;
 	private static UserDTO dummyTransfer;
 	static List<User> dummyDB;
 	
-	@Mock(name="RoleRepo")
-	private static UserRoleRepository mockrole;
-	
-	@InjectMocks
-	@Qualifier(value="RoleRepo")
-	private static UserRoleServiceImpl rServ;
-	
+		
 	private static UserRole role;
 	static List<UserRole> dummyRoleDB;
 
 	Logger log = Logger.getLogger(UserServiceTest.class);
 	
+	private static Field f;
+	private static Field f2;
+	
 	@BeforeAll
 	static void setupBefore() throws Exception {
 		mockrepo = Mockito.mock(UserRepository.class);
-		uServ = new UserServiceImpl();
-		
 		mockrole = Mockito.mock(UserRoleRepository.class);
-		rServ = new UserRoleServiceImpl();
+		uServ = new UserServiceImpl();
 
+		f = uServ.getClass().getDeclaredField("userRoleService");
+		f.setAccessible(true);
+		f2 = f.getClass().getDeclaredField("repository");
+		f2.setAccessible(true);
+		
+		f2.set(UserRoleRepository.class, mockrole);
+		
 		role = new UserRole();
 		role.setId(0);
 		role.setName("Shopper");
@@ -78,13 +85,9 @@ public class UserServiceTest {
 		u1.setEmail("test@gmail.com");
 		u1.setFirstName("Levi");
 		u1.setLastName("Choi");
-		u1.setPassword("Badpassword");
+		u1.setPassword("Badpassword");		
+		u1.setUserRole(role);
 		
-		
-		//sanity test
-		mockrole.findById(0);
-		rServ.add(role);
-		u1.setUserRole(rServ.getById(0));
 		tempId = UUID.fromString("22c2db34-749c-45f0-a066-f3bd03bad995");
 		u2 = new User();
 		u2.setId(tempId);
@@ -92,7 +95,7 @@ public class UserServiceTest {
 		u2.setFirstName("Adam");
 		u2.setLastName("Lucas");
 		u2.setPassword("goodP@ssword");
-		u1.setUserRole(rServ.getById(0));
+		u1.setUserRole(role);
 		
 		dummyTransfer = new UserDTO();
 		
@@ -100,13 +103,18 @@ public class UserServiceTest {
 		dummyDB.add(u1);
 		dummyDB.add(u2);
 	}
+	@AfterAll
+	static void afterTesting() {
+		
+	}
 	
 	@Test
 	@Order(1)
 	@DisplayName("1. Mock Validation")
-	void checkMockInjection() {
+	void checkMockInjection() throws IllegalArgumentException, IllegalAccessException {
 		assertThat(mockrepo).isNotNull();
 		assertThat(uServ).isNotNull();
+		assertThat(f2.get(mockrole)).isNotNull();
 	}
 	
 	@Test
@@ -121,7 +129,7 @@ public class UserServiceTest {
 		u3.setFirstName("Yugal");
 		u3.setLastName("Subedi");
 		u3.setPassword("N3Wpassword");
-		u3.setUserRole(rServ.getById(0));
+		u3.setUserRole(role);
 		
 		when(mockrepo.save(u3)).thenReturn(u3);
 		
