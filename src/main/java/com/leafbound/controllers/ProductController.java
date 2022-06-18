f04c48b90ca5515a77b594855566d74109615729
FullSol <ceraines@student.fullsail.edu>
2022-06-18 14:23:57 -0400

    changed put mapping to patch mappings

--- src/main/java/com/leafbound/controllers/ProductController.java
+++ src/main/java/com/leafbound/controllers/ProductController.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -46,7 +46,7 @@ public class ProductController {
 	}
 
 	// Update product
-	@PutMapping("/product")
+	@PatchMapping("/product")
 	@ApiOperation(value = "update product entity")
 	public @ResponseBody Product updateProduct(@RequestBody Product product) {
 		return productServ.updateProduct(product);
