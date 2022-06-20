a2f4c098be9219503d33ccb6ca8c813944586860
Vidya Bineesh <vidya902@revature.net>
2022-06-20 18:14:24 -0400

    Fixed two sonarcloud issues found in order controller

--- src/main/java/com/leafbound/LeafboundApplication.java
+++ src/main/java/com/leafbound/LeafboundApplication.java
@@ -1,10 +1,17 @@
 package com.leafbound;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LeafboundApplication {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LeafboundApplication.class, args);
